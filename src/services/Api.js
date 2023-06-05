707a3529a41e1936e3a6c619c19da1772b3f10a7
vya-vyu <linda1307@gmail.com>
2023-06-05 19:36:21 +0100

    Updates

--- src/services/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://64787b3e362560649a2ddef5.mockapi.io";
-
-export const getUsersApi = () => axios.get("/users").then(({ data }) => data);
-export const replaceFollowersApi = (id) =>
-  axios.put(`users/${id}`).then((res) => res);
